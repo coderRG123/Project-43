6b9e989785d94f26d3d856f782e1ee8afda2caaa
coderRG123 <76978363+coderRG123@users.noreply.github.com>
2021-06-17 11:17:35 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    fill("white")
+    text("A T A D A S B A", 700, 150);
+    fill("lightblue")
+    text("Hint: Stores All Information!!", 700,170)
+    fill("white")
+    text("C U T N I F O N", 400, 250);
+    fill("lightblue")
+    text("Hint: Performs A Particular Task!!", 400,270)
+
+
+    
+
+}
